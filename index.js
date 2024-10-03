4720ed99cba0fb49138b378bdb21989fde08b8ca
TomasHo <qjin123456789@gmail.com>
2024-10-03 19:32:57 +0200

    testing git thats it

---
+++ index.js
@@ -0,0 +1,31 @@
+const myCheckBox = document.getElementById("myCheckBox")
+const visBtn = document.getElementById("visaBtn")
+const mastercardBtn = document.getElementById("masterCardBtn")
+const payPalBtn = document.getElementById("payPalBtn")
+const mySubmit = document.getElementById("mySubmit")
+const subResult = document.getElementById("subResult")
+const paymentResult = document.getElementById("paymenResult")
+
+mySubmit.onclick = function(){
+    if(myCheckBox.checked){
+        subResult.textContent = "you are subscribed";
+    }
+    else{
+        subResult.textContent = "you are NOT subscribed";
+    }
+
+    if(visBtn.checked){
+        paymentResult.textContent = "you are paying with visa"
+    }
+    else if(mastercardBtn.checked){
+        paymentResult.textContent = "you are paying with Master Card";
+    }
+    else if(payPalBtn.checked){
+        paymentResult.textContent = "you are paying with paypal";
+    }
+    else{
+        paymentResult.textContent = "you must select a payment type";
+    }
+}
+
+//testing git
