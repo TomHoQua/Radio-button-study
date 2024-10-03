68f74669f1c2c1d5d874a41f0e42c3d8709647ce
TomasHo <qjin123456789@gmail.com>
2024-10-03 19:06:13 +0200

    javascript function added

---
+++ index.js
@@ -0,0 +1,29 @@
+const myCheckBox = document.getElementById("myCheckBox")
+const visBtn = document.getElementById("visaBtn")
+const mastercardBtn = document.getElementById("masterCardBtn")
+const payPalBtn = document.getElementById("payPalBtn")
+const mySubmit = document.getElementById("mySubmit")
+const subResult = document.getElementById("subResult")
+const paymentResult = document.getElementById("paymenResult")
+
+mySubmit.onclick = function(){
+    if(myCheckBox.checked){
+        subResult.textContent = "you are subscribed";
+    }
+    else{
+        subResult.textContent = "you are NOT subscribed";
+    }
+
+    if(visBtn){
+        paymentResult.textContent = "you are paying with visa"
+    }
+    else if(mastercardBtn){
+        paymentResult.textContent = "you are paying with Master Card";
+    }
+    else if(payPalBtn){
+        paymentResult.textContent = "you are paying with paypal";
+    }
+    else{
+        paymentResult.textContent = "you must select a payment type";
+    }
+}
